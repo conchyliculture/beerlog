50cdff7ccb7a69ab00700e9a1ec1d4714c947969
renzo <github@renzokuken.eu>
2018-10-04 00:17:58 +0200

    add more silly tests

--- tests.py
+++ tests.py
@@ -2,9 +2,14 @@
 
 from __future__ import print_function
 
+import json
+import tempfile
 import unittest
 
+from beerlog import BeerLog
 from beerlogdb import BeerLogDB
+
+# pylint: disable=protected-access
 
 class TestBeerLogDB(unittest.TestCase):
   """Tests for beerlogdb module."""
@@ -21,6 +26,26 @@ class TestBeerLogDB(unittest.TestCase):
     saved_entry = db.AddEntry(character='pute', pic='race.jpg')
     saved_entry = db.AddEntry(character='pute', pic='race.jpg')
     self.assertEqual(3, db.CountAll())
+
+class TestBeerLog(unittest.TestCase):
+  """Tests for beerlog module."""
+
+  def testBeerLog(self):
+    """Simple test case."""
+    bl_object = BeerLog()
+
+    with tempfile.NamedTemporaryFile() as temp:
+      temp.write(json.dumps({'0x0':{'name': 'Kikoo'}}))
+      temp.flush()
+      bl_object._known_tags = temp.name
+      bl_object.LoadTagsDB()
+      l = bl_object.known_tags_list
+      self.assertEqual(1, len(l))
+
+      self.assertEqual('Kikoo', bl_object.GetNameFromTag('0x0'))
+
+      self.assertEqual(None, bl_object.GetNameFromTag('0x1'))
+
 
 if __name__ == '__main__':
   unittest.main()

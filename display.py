6a42852e63e6c7174ba0afc7dda224759cf7d340
renzo <github@renzokuken.eu>
2018-10-09 00:07:02 +0200

    basic menu in emulator

---
+++ display.py
@@ -0,0 +1,83 @@
+# pylint: disable=missing-docstring
+
+from __future__ import print_function
+
+import sys
+from PIL import ImageFont
+
+from luma.core.render import canvas as LumaCanvas
+from luma.emulator import device
+
+import pygame
+import pygame.key
+
+
+class Display(object):
+
+  MENU_ITEMS = ['un', 'deux', 'trois', 'douze']
+
+  MENU_TEXT_X = 2
+  MENU_TEXT_HEIGHT = 10
+
+  def __init__(self, luma_device):
+    self._menu_index = 0
+    self._text_font = ImageFont.load_default()
+
+    self.luma_device = luma_device
+
+  def _DrawMenuItem(self, drawer, number):
+    selected = self._menu_index == number
+    rectangle_geometry = (
+        self.MENU_TEXT_X,
+        number * self.MENU_TEXT_HEIGHT,
+        self.luma_device.width,
+        ((number+1) * self.MENU_TEXT_HEIGHT)
+        )
+    text_geometry = (
+        self.MENU_TEXT_X,
+        number*self.MENU_TEXT_HEIGHT
+        )
+    if selected:
+      drawer.rectangle(
+          rectangle_geometry, outline='white', fill='white')
+      drawer.text(
+          text_geometry,
+          self.MENU_ITEMS[number],
+          font=self._text_font, fill='black'
+          )
+    else:
+      drawer.text(
+          text_geometry,
+          self.MENU_ITEMS[number],
+          font=self._text_font, fill='white')
+
+  def DrawMenu(self):
+    with LumaCanvas(self.luma_device) as drawer:
+      drawer.rectangle(
+          self.luma_device.bounding_box, outline="white", fill="black")
+      for i in range(len(self.MENU_ITEMS)):
+        self._DrawMenuItem(drawer, i)
+
+  def MenuDown(self):
+    self._menu_index = ((self._menu_index + 1)%len(self.MENU_ITEMS))
+    self.DrawMenu()
+
+  def MenuUp(self):
+    self._menu_index = ((self._menu_index - 1)%len(self.MENU_ITEMS))
+    self.DrawMenu()
+
+
+if __name__ == '__main__':
+  m = Display(device.pygame())
+  m.DrawMenu()
+  while True:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        sys.exit()
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_UP:
+          m.MenuUp()
+        elif event.key == pygame.K_DOWN:
+          m.MenuDown()
+
+# vim: tabstop=2 shiftwidth=2 expandtab

86f56d30fdbd438e7dde6d1338b3c72b90e9e2e0
frenchsplainer <6418006+conchyliculture@users.noreply.github.com>
2019-09-01 15:29:33 +0200

    add the system module

---
+++ beerlog/system.py
@@ -0,0 +1,22 @@
+"""Helper methods for collecting some system data."""
+
+import logging
+import re
+import subprocess
+
+
+def GetWifiStatus():
+  """Return a short string with the WiFi status."""
+  result = 'No WiFi'
+
+  regexp = re.compile(r'^([^ ]+)\s+ESSID:"(.+)"$')
+
+  try:
+    output = subprocess.check_output(['/sbin/iwgetid']).decode('utf-8')
+    match = regexp.search(output)
+    if match:
+      _, essid = match.groups()
+      result = essid
+  except Exception as e: # pylint: disable=broad-except
+    logging.warning(e)
+  return result

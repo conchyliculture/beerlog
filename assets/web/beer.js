5819dde4040901ffbbee8e25b397e8117bdedb96
Lionel Landwerlin <llandwerlin@gmail.com>
2020-08-17 13:44:07 +0200

    web: generate colors using hue/saturation/lightness (#28)

--- assets/web/beer.js
+++ assets/web/beer.js
@@ -15,28 +15,11 @@ function drawChart(le_data) {
     var datasets = le_data['datasets']
     console.log(datasets);
 
-    var colors = [
-        "rgb(255, 87, 87)", // red
-        "rgb(255, 171, 87)", // orange
-        "rgb(255, 213, 87)", // orange clair
-        "rgb(213, 255, 87)", // yellow
-        "rgb(171, 255, 87)", // vert clair
-        "rgb(87, 255, 87)", // vert
-        "rgb(87, 255, 171)", // turquoise
-        "rgb(87, 255, 255)", // cyan
-        "rgb(87, 171, 255)", // bleu
-        "rgb(87, 87, 255)", // indigo
-        "rgb(171, 87, 255)", // purple
-        "rgb(255, 87, 255)", // magenta
-        "rgb(255, 87, 171)", // rose
-        "rgb(128, 128, 128)", // grey
-        "rgb(50, 55, 55)", // light grey
-    ];
-
     datasets.forEach(function (item, index) {
+        var color = "hsl(" + (360.0 * index / datasets.length) + ", 50%, 50%)";
         item.fill = false;
-        item.backgroundColor = colors[index];
-        item.borderColor = colors[index];
+        item.backgroundColor = color;
+        item.borderColor = color;
     });
 
     var ctx = document.getElementById('myChart').getContext('2d');

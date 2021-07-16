ca984387ea4a168dc5c4f002e00e5d590c81fb83
frenchsplainer <6418006+conchyliculture@users.noreply.github.com>
2021-07-16 12:13:47 +0200

    remove lame

--- tools/web.py
+++ tools/web.py
@@ -38,7 +38,6 @@ class Handler(http.server.BaseHTTPRequestHandler):
 </body></html> """
 
   def __init__(self, *args, **kwargs):
-    self._datasets = None
     self._db = None
     self._characters = None
     self._Setup()
@@ -74,10 +73,7 @@ class Handler(http.server.BaseHTTPRequestHandler):
       self.send_response(200)
       self.send_header('Content-type', 'application/json')
       self.end_headers()
-      if self.options.lame:
-        self.wfile.write(self.GetDataLame())
-      else:
-        self.wfile.write(self.GetData())
+      self.wfile.write(self.GetData())
     else:
       self.send_error(404, 'error')
 
@@ -124,63 +120,6 @@ class Handler(http.server.BaseHTTPRequestHandler):
             'total': total}}
         ).encode()
 
-  def GetDataLame(self):
-    """Builds a dict to use with Chart.js, but in a lame way.
-
-    This means returning a graph where all scan is a node."""
-    data = {}
-    for character in self._characters:
-      for row in self._db.GetDataFromName(character):
-        data[row.timestamp.strftime('%Y-%m-%d %H:%M:%S')] = (character, row.sum)
-
-    # {
-    #    time1: (char_1, cummulated),
-    #    time2: (char_2, cummulated),
-    #    time3: (char_1, cummulated),
-    #    ....
-    # }
-
-    # First, add a data point before the first scan
-    labels = [(
-        self._db.GetEarliestTimestamp()-datetime.timedelta(hours=1)
-        ).strftime('%Y-%m-%d %H:%M:%S')]
-    datasets = []
-    for char in self._characters:
-      datasets.append({
-            'label': char,
-            'data': [0],
-            'steppedLine': True,
-            'spanGaps': True}) # Draw 'null' values
-    for time in sorted(data):
-      labels.append(time)
-      character, amount = data[time]
-
-      for dataset in datasets:
-        if dataset['label'] == character:
-          dataset['data'].append(amount)
-        else:
-          dataset['data'].append(None)
-
-    # We need one last datapoint to draw all the way to the end of times
-    labels.append((
-        self._db.GetLatestTimestamp()+datetime.timedelta(hours=1)
-        ).strftime('%Y-%m-%d %H:%M:%S'))
-    for dataset in datasets:
-      i = -1
-      # Search for the last non empty entry
-      while dataset['data'][i] is None:
-        i -= 1
-      dataset['data'].append(dataset['data'][i])
-
-    return json.dumps(
-        {'data':{
-            'labels': labels,
-            'datasets': datasets,
-            'drinkers': self._characters,
-            'total': self._db.GetTotalAmount() / 100.0}}
-        ).encode()
-
-
 def ParseArguments():
   """Parses arguments.
 
@@ -197,9 +136,6 @@ def ParseArguments():
       '--known_tags', dest='known_tags', action='store',
       default=DEFAULT_TAGS_FILE,
       help='the known tags file to use to use')
-  parser.add_argument(
-      '--lame', dest='lame', action='store_true',
-      help='uses a lame method for graphs, by plotting NFC scans as nodes')
   parser.add_argument(
       '--port', dest='port', action='store',
       default=DEFAULT_PORT, type=int,
